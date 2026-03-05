bf1d9483b011f153fc9c11d9d32f77582eb15247
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-05 13:51:01 +0700

    feat : segitiga number

--- nomor1.js
+++ nomor1.js
@@ -1,6 +1,11 @@
-number = 2
-// console.log(typeof number)
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+const rl = readline.createInterface({ input, output });
+
 numArr = []
+
+rl.question('angka ', (number) => {
+
 function ceking(num){
     if (typeof number && number > 0 ){
     for (let i = 1; i <= number; i++) {
@@ -8,12 +13,13 @@ function ceking(num){
       console.log(numArr.join(" "))
 
     }
-}
+    }
     else{
-  console.log("Parameter Harus angka dan harus nomor positif")
+    console.log("Parameter Harus angka dan harus nomor positif")
   }
-
 }
 
-ceking(number)
+  ceking(number)
 
+  rl.close();
+});
