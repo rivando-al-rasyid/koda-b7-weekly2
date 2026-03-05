6e538fe5f70f2b70279a2a16fa0c5c299e98eebf
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-05 15:25:23 +0700

    feat: proses number dengan perkalian angka

---
+++ nomor3.js
@@ -0,0 +1,21 @@
+function processNumber(array){
+  let len = array.length;
+  let arrayBaru = [];
+  for (let i = 0; i < len; i++) {
+    cekModulus = array[i] % 2
+    if (cekModulus === 1){arrayBaru.push(array[i])}
+  }
+  if (arrayBaru === []){
+    console.log("Ouput : 1")
+  }
+  else{
+    let hasil = 1
+    arrayBaru.forEach(num => {
+    hasil *= num;
+    });
+    console.log(hasil)
+  }
+
+}
+
+processNumber([2,3,5,8])
